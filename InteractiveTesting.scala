d55d2dfca5c93c9d347d1a859147a180d91614c6
Vince <vince.reuter@gmail.com>
2020-02-17 16:13:45 -0500

    test data seeking writing; organize

--- InteractiveTesting.scala
+++ InteractiveTesting.scala
@@ -1,19 +1,25 @@
 object InteractiveTesting {
-  import java.io.File
+  
+  import java.io.{ BufferedWriter =>  BW, File, FileWriter => FW }
+  import java.nio.file.Paths
   val libpath = "/home/vr/code/autoquiz/target/scala-2.12/autoquiz_v0.0.2-SNAPSHOT.jar"
   interp.load.cp(ammonite.ops.Path(libpath))
   import autoquiz._
   interp.load.ivy("org.typelevel" %% "cats-core" % "2.1.0")
   import cats.data.{ NonEmptyList => NEL }
   import cats.syntax.list._
+  interp.load.ivy("org.typelevel" % "mouse_2.12" % "0.19")
+  import mouse.boolean._
   interp.load.ivy("io.circe" %% "circe-core" % "0.12.3")
   import io.circe._
+  
   def parse2Nel: Either[Error, List[TexQA]] => NEL[TexQA] = _.toOption.get.toNel.get
   val testIn1 = new File("/home/vr/code/mbgene/07_TechniquesOfMolecularBiology/07-General.QandA.json")
   val qaG1 = Parsing.readFile(testIn1).toOption.get.toNel.get
   val testIn2 = new File("/home/vr/code/mbgene/07_TechniquesOfMolecularBiology/07-EMSA.QandA.json")
   val qaG2 = Parsing.readFile(testIn2).toOption.get.toNel.get
   val testOut1 = new File("/home/vr/testtex/testQaOut1.tex")
+  
   def standardPreamble(title: String, author: String, date: String) = List(
     "\\documentclass{article}", 
     "\\usepackage{amsmath, amssymb}", 
@@ -32,7 +38,46 @@ object InteractiveTesting {
     "\\maketitle", 
     ""
   ) mkString "\n"
+  
   implicit val myRender = RenderQA(
     (q: String) => s"\\item{$q}", "QandA", "\\begin{answered}", "\\end{answered}")
+  
   Writing.writeTexQA( testOut1, standardPreamble("Test", "Vince", "Feb. 16"), NEL("General" -> qaG1, List("EMSA" -> qaG2)) )
+
+  def writeAllQA: (Option[File], List[File], List[(File, String)]) = {
+    import cats.Alternative, cats.instances.either._, cats.instances.list._
+    val ext = ".QandA.json"
+    def file2Name(f: File): String = {
+      val nDot = f.getName.count(_ == '.')
+      val fnFields = f.getName.split("\\.").dropRight(nDot).mkString(".").split("-").toList
+      fnFields.head.forall(Character.isDigit).fold(fnFields.tail, fnFields) mkString " "
+    }
+    val folders = List("mbgene", "AlbertsMolbio") map {
+      foldName => new File(Paths.get(System.getenv("CODE"), foldName).toString) }
+    val sectFpairs = folders flatMap { DataSeek.seekData(_: File, NEL(ext, List()), file2Name) }
+    val (errFilePairs, secFileGroupTrios) = Alternative[List].separate(
+      sectFpairs map { case (sect, f) =>  Parsing.readFile(f) match {
+        case Left(e) => Left[(File, String), (String, File, NEL[TexQA])](f -> e.getMessage)
+        case Right(qas) => qas.toNel match {
+          case None => Left[(File, String), (String, File, NEL[TexQA])](f -> "Empty parse")
+          case Some(res) => Right[(File, String), (String, File, NEL[TexQA])]((sect, f, res))
+        }
+      } }
+    )
+    secFileGroupTrios.toNel match {
+      case None => (Option.empty[File], List.empty[File], errFilePairs)
+      case Some(trios) => {
+        val outfile = new File("/home/vr/testtex/testQaOutAll.tex")
+        val preamble = standardPreamble("All Questions", "Vince Reuter", "February 17, 2020")
+        val (files, groups) = trios.toList.foldRight(
+          List.empty[File] -> List.empty[(String, NEL[TexQA])]){ 
+            case ((n, f, qas), (fs, gs)) => (f :: fs, (n, qas) :: gs) }
+        Writing.writeTexQA(outfile, preamble, groups.toNel.get)
+        (Some(outfile), files, errFilePairs)
+      }
+    }
+  }
+
+  val (maybeTestF, processedInfiles, inErrPairs) = writeAllQA
+
 }
