5059265328adcca1469781f9b248782e2d7a91c8
Vince <vince.reuter@gmail.com>
2020-02-22 10:39:59 -0500

    start new version

--- InteractiveTesting.scala
+++ InteractiveTesting.scala
@@ -2,7 +2,7 @@ object InteractiveTesting {
   
   import java.io.{ BufferedWriter =>  BW, File, FileWriter => FW }
   import java.nio.file.Paths
-  val libpath = "/home/vr/code/autoquiz/target/scala-2.12/autoquiz_v0.0.4.jar"
+  val libpath = "/home/vr/code/autoquiz/target/scala-2.12/autoquiz_v0.0.5-SNAPSHOT.jar"
   interp.load.cp(ammonite.ops.Path(libpath))
   import autoquiz._, Targets.{ TargetFolder, Relpath }
   interp.load.ivy("org.typelevel" %% "cats-core" % "2.1.0")
--- build.sbt
+++ build.sbt
@@ -1,5 +1,5 @@
 name := "autoquiz"
-version := "0.0.4"
+version := "0.0.5-SNAPSHOT"
 scalaVersion := "2.12.9"
 organization := "vreuter"
 
