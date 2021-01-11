947e6f75c6287a146a512eb58b65b1725fdf61a0
Vince Reuter <vince.reuter@gmail.com>
2021-01-11 16:50:16 -0500

    implementing codecs for new ADTs

--- src/main/scala/Conceptual.scala
+++ src/main/scala/Conceptual.scala
@@ -2,6 +2,7 @@ package autoquiz
 
 object Conceptual {
   import cats.data.{ NonEmptyList => NEL }
+  import io.circe._, io.circe.parser._, io.circe.syntax._
   import Support.SupportSourceLike
   
   sealed trait ShortTextLike
@@ -11,6 +12,13 @@ object Conceptual {
   
   object ShortTextLike {
     def nothing: ShortTextLike = ShortNull
+    implicit val shortTextLikeCodec: Codec[ShortTextLike] = new Codec[ShortTextLike] {
+      def apply(t: ShortTextLike): Json = t match {
+        case ShortNull => Json.Null
+        case ShortText(t) => t.asJson
+      }
+      def apply(c: HCursor): Decoder.Result[ShortTextLike] = c.as[String].map(t => ShortText(t))
+    }
   }
   
   sealed trait LongTextLike
@@ -20,6 +28,13 @@ object Conceptual {
   
   object LongTextLike {
     def nothing: LongTextLike = LongNull
+    implicit val longTextLikeCodec: Codec[LongTextLike] = new Codec[LongTextLike] {
+      def apply(t: LongTextLike): Json = t match {
+        case LongNull => Json.Null
+        case LongText(t) => t.asJson
+      }
+      def apply(c: HCursor): Decoder.Result[LongTextLike] = c.as[String].map(t => LongText(t))
+    }
   }
 
   final case class Concept(
@@ -35,6 +50,13 @@ object Conceptual {
       apply(name, name, formula, NEL(statement, List()), ShortTextLike.nothing, LongTextLike.nothing, List(support))
     def apply(key: String, name: String, formula: String, statement: String, support: SupportSourceLike): Concept = 
       new Concept(key, name, formula, NEL(statement, List()), ShortTextLike.nothing, LongTextLike.nothing, List(support))
+    /*implicit val conceptCodec: Codec[Concept] = new Codec[Concept] {
+      def apply(c: Concept): Json = {
+        Json("key" -> c.key.asJson, fo)
+      }
+      def apply(c: HCursor): Decoder.Result[Concept] = ???
+    }
+    */
   }
 
 }
--- src/main/scala/Support.scala
+++ src/main/scala/Support.scala
@@ -3,18 +3,79 @@ package autoquiz
 object Support {
   import java.io.File
   import cats.data.{ NonEmptyList => NEL }
+  import cats.syntax.list._
+  import mouse.boolean._
+  import io.circe._, io.circe.parser._, io.circe.syntax._
   
   sealed trait SupportSourceLike
-  
-  final case class ImageFile(get: File) extends SupportSourceLike {
-    require(get.isFile, s"Alleged image file isn't a file: $get")
+
+  private[this] def unifyDecoders[A, B, U](dA: Decoder[A], dB: Decoder[B]): Decoder[U] = 
+    dA.either(dB).map(_.fold(_.asInstanceOf[U], _.asInstanceOf[U]))
+
+  object SupportSourceLike {
+    implicit val supportLikeCodec: Codec[SupportSourceLike] = new Codec[SupportSourceLike] {
+      import ProofLines._, SupportFile._
+      def apply(ssl: SupportSourceLike): Json = ssl match {
+        case pfl: ProofLines => pfl.asJson
+        case sf: SupportFile => sf.asJson
+      }
+      def apply(c: HCursor): Decoder.Result[SupportSourceLike] = 
+        unifyDecoders(implicitly[Decoder[SupportFile]], implicitly[Decoder[ProofLines]])(c)
+    }
+  }
+
+  private implicit val extantFileCodec: Codec[File] = new Codec[File] {
+    def apply(f: File): Json = f.getPath.asJson
+    def apply(c: HCursor): Decoder.Result[File] = c.as[String].map(
+      p => new File(p)).flatMap(f => f.isFile.either(DecodingFailure(s"Missing file: $f", Nil), f))
+  }
+
+  sealed trait SupportFile extends SupportSourceLike
+  object SupportFile {
+    implicit val supportFileCodec: Codec[SupportFile] = new Codec[SupportFile] {
+      import ImageFile._, LatexFile._
+      def apply(sf: SupportFile): Json = sf match {
+        case f: LatexFile => f.asJson
+        case f: ImageFile => f.asJson
+      }
+      def apply(c: HCursor): Decoder.Result[SupportFile] = 
+        unifyDecoders(implicitly[Decoder[LatexFile]], implicitly[Decoder[ImageFile]])(c)
+    }
+  }
+
+  final case class ImageFile(image: File) extends SupportFile {
+    require(get.isFile, s"Alleged image file isn't a file: $image")
+    final def get: File = image
+  }
+  object ImageFile {
+    private[this] val key = "image"
+    implicit val imageFileCodec: Codec[ImageFile] = new Codec[ImageFile] {
+      def apply(f: ImageFile): Json = Json.obj(key -> f.get.asJson)
+      def apply(c: HCursor): Decoder.Result[ImageFile] = c.get[File](key).map(f => ImageFile(f))
+    }
   }
   
-  final case class LatexFile(get: File) extends SupportSourceLike {
+  final case class LatexFile(latex: File) extends SupportFile {
     require(get.isFile, s"Alleged image file isn't a file: $get")
+    final def get: File = latex
+  }
+  object LatexFile {
+    private[this] val key = "latex"
+    implicit val latexFileCodec: Codec[LatexFile] = new Codec[LatexFile] {
+      def apply(f: LatexFile): Json = Json.obj(key -> f.asJson)
+      def apply(c: HCursor): Decoder.Result[LatexFile] = c.get[File](key).map(f => LatexFile(f))
+    }
   }
   
   final case class ProofLines(get: NEL[String]) extends SupportSourceLike {
     final def list: List[String] = get.toList
+  }
+  
+  object ProofLines {
+    implicit val proofLinesCodec: Codec[ProofLines] = new Codec[ProofLines] {
+      def apply(lines: ProofLines): Json = lines.get.toList.asJson
+      def apply(c: HCursor): Decoder.Result[ProofLines] = c.as[List[String]].flatMap(
+        _.toNel.toRight(DecodingFailure("Empty proof lines", Nil)).map(ls => ProofLines(ls)))
+    }
   }
 }
